cc0b9af8b79934597fdacd86890223a243ac0a09
ShuangLiu <liu.shuang@huasheng.io>
2022-04-13 22:53:16 +0800

    clean code

--- src/util/math.ts
+++ src/util/math.ts
@@ -1,5 +1,8 @@
 import { mat4, vec3 } from 'gl-matrix'
 
+const fov:number = (2 * Math.PI) / 5
+const near:number = 1.0
+const far:number = 100.0
 // return mvp matrix from given aspect, position, rotation, scale
 function getMvpMatrix(
     aspect: number,
@@ -7,10 +10,7 @@ function getMvpMatrix(
     rotation: {x:number, y:number, z:number},
     scale: {x:number, y:number, z:number}
 ){
-    // create a perspective Matrix
-    const projectionMatrix = mat4.create()
-    mat4.perspective(projectionMatrix, (2 * Math.PI) / 5, aspect, 1, 100.0)
-    // create modelView Matrix
+    // get modelView Matrix
     const viewMatrix = mat4.create()
     // translate position
     mat4.translate(viewMatrix, viewMatrix, vec3.fromValues(positioin.x, positioin.y, positioin.z))
@@ -20,9 +20,15 @@ function getMvpMatrix(
     mat4.rotateZ(viewMatrix, viewMatrix, rotation.z)
     // scale
     mat4.scale(viewMatrix, viewMatrix, vec3.fromValues(scale.x, scale.y, scale.z))
-    // create mvp matrix
+    
+    // get a perspective Matrix
+    const projectionMatrix = mat4.create()
+    mat4.perspective(projectionMatrix, fov, aspect, near, far)
+    
+    // get mvp matrix
     const modelViewProjectionMatrix = mat4.create()
     mat4.multiply(modelViewProjectionMatrix, projectionMatrix, viewMatrix)
+    
     // return matrix as Float32Array
     return modelViewProjectionMatrix as Float32Array
 }
