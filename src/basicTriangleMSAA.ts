66f80b1c54dd6857bb3493b5ac4092ac83a8e09c
ShuangLiu <liu.shuang@huasheng.io>
2022-05-19 18:23:50 +0800

    add MSAA demo

---
+++ src/basicTriangleMSAA.ts
@@ -0,0 +1,117 @@
+import triangleVert from './shaders/triangle.vert.wgsl?raw'
+import redFrag from './shaders/red.frag.wgsl?raw'
+
+// initialize webgpu device & config canvas context
+async function initWebGPU(canvas: HTMLCanvasElement) {
+    if(!navigator.gpu)
+        throw new Error('Not Support WebGPU')
+    const adapter = await navigator.gpu.requestAdapter({
+        powerPreference: 'high-performance'
+        // powerPreference: 'low-power'
+    })
+    if (!adapter)
+        throw new Error('No Adapter Found')
+    const device = await adapter.requestDevice()
+    const context = canvas.getContext('webgpu') as GPUCanvasContext
+    const format = context.getPreferredFormat(adapter)
+    const devicePixelRatio = window.devicePixelRatio || 1
+    const size = {
+        width: canvas.clientWidth * devicePixelRatio,
+        height: canvas.clientHeight * devicePixelRatio
+    }
+    context.configure({
+        // json specific format when key and value are the same
+        device, format, size,
+        // prevent chrome warning
+        compositingAlphaMode: 'opaque'
+    })
+    return {device, context, format, size}
+}
+// create a simple pipiline with multiSample
+async function initPipeline(device: GPUDevice, format: GPUTextureFormat): Promise<GPURenderPipeline> {
+    const descriptor: GPURenderPipelineDescriptor = {
+        layout: 'auto',
+        vertex: {
+            module: device.createShaderModule({
+                code: triangleVert
+            }),
+            entryPoint: 'main'
+        },
+        primitive: {
+            topology: 'triangle-list' // try point-list, line-list, line-strip, triangle-strip?
+        },
+        fragment: {
+            module: device.createShaderModule({
+                code: redFrag
+            }),
+            entryPoint: 'main',
+            targets: [
+                {
+                    format: format
+                }
+            ]
+        },
+        multisample: {
+            count: 4,
+        }
+    }
+    return await device.createRenderPipelineAsync(descriptor)
+}
+// create & submit device commands
+function draw(device: GPUDevice, context: GPUCanvasContext, pipeline: GPURenderPipeline, MSAAView: GPUTextureView) {
+    const commandEncoder = device.createCommandEncoder()
+    const renderPassDescriptor: GPURenderPassDescriptor = {
+        colorAttachments: [
+            {
+                view: MSAAView,
+                resolveTarget: context.getCurrentTexture().createView(),
+                clearValue: { r: 0, g: 0, b: 0, a: 1.0 },
+                loadOp: 'clear', // clear/load
+                storeOp: 'store' // store/discard
+            }
+        ]
+    }
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor)
+    passEncoder.setPipeline(pipeline)
+    // 3 vertex form a triangle
+    passEncoder.draw(3)
+    passEncoder.end()
+    // webgpu run in a separate process, all the commands will be executed after submit
+    device.queue.submit([commandEncoder.finish()])
+}
+
+async function run(){
+    const canvas = document.querySelector('canvas')
+    if (!canvas)
+        throw new Error('No Canvas')
+    const {device, context, format, size} = await initWebGPU(canvas)
+    const pipeline = await initPipeline(device, format)
+    // create 4x samplecount texture
+    let MSAATexture = device.createTexture({
+        size, format,
+        sampleCount: 4,
+        usage: GPUTextureUsage.RENDER_ATTACHMENT
+    });
+    let MSAAView = MSAATexture.createView();
+    // start draw
+    draw(device, context, pipeline, MSAAView)
+    
+    // re-configure context on resize
+    window.addEventListener('resize', ()=>{
+        size.width = canvas.clientWidth * devicePixelRatio
+        size.height = canvas.clientHeight * devicePixelRatio
+        context.configure({
+            device, format, size,
+            compositingAlphaMode: 'opaque'
+        })
+        MSAATexture.destroy()
+        MSAATexture = device.createTexture({
+            size, format,
+            sampleCount: 4,
+            usage: GPUTextureUsage.RENDER_ATTACHMENT
+        });
+        MSAAView = MSAATexture.createView();
+        draw(device, context, pipeline, MSAAView)
+    })
+}
+run()
