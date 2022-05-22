5ba367138c5d2653adb6ce780947ac173f7635ed
ShuangLiu <liu.shuang@huasheng.io>
2022-05-22 20:34:36 +0800

    update WebGPU origin-token

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 // vite.config.js
 import { defineConfig } from 'vite'
-const devToken = 'Amu7sW/oEH3ZqF6SQcPOYVpF9KYNHShFxN1GzM5DY0QW6NwGnbe2kE/YyeQdkSD+kZWhmRnUwQT85zvOA5WYfgAAAABJeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJmZWF0dXJlIjoiV2ViR1BVIiwiZXhwaXJ5IjoxNjUyODMxOTk5fQ=='
+const devToken = 'AjPwILqou86MNqXlfZc0tZycsl9U9sV/uI2ti0RK1/w0kT3/l35O3zugkEb31z1gKbxnakvZahtfWf9h42buSA4AAABJeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJmZWF0dXJlIjoiV2ViR1BVIiwiZXhwaXJ5IjoxNjYzNzE4Mzk5fQ=='
 
 module.exports = defineConfig({
     plugins: [
