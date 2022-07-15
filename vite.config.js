fb2d2453c1f0f40f3ff563b5291723af696b9932
ShuangLiu <liu.shuang@huasheng.io>
2022-07-15 14:43:20 +0800

    upgrade to vite3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite'
 const devToken = 'AjPwILqou86MNqXlfZc0tZycsl9U9sV/uI2ti0RK1/w0kT3/l35O3zugkEb31z1gKbxnakvZahtfWf9h42buSA4AAABJeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJmZWF0dXJlIjoiV2ViR1BVIiwiZXhwaXJ5IjoxNjYzNzE4Mzk5fQ=='
 
 module.exports = defineConfig({
+    server:{
+        port: 3000
+    },
     plugins: [
         {
             name: 'Origin-Trial',
