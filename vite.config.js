858cfa78ada7edaca77fdac2a16210ccfb4987be
ShuangLiu <liu.shuang@huasheng.io>
2022-07-28 18:35:17 +0800

    use localhost as default

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 // vite.config.js
 import { defineConfig } from 'vite'
+import dns from 'dns'
+dns.setDefaultResultOrder('verbatim')
+
 const devToken = 'AlsgHBaJPdlZ24pkroBSkRHFeYGm+p7QxSiR0reBTV2f60MRKX1GxaJzJHIljZNapPCIuz7+mIGQ2xQFKEaUTgYAAABJeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJmZWF0dXJlIjoiV2ViR1BVIiwiZXhwaXJ5IjoxNjc1MjA5NTk5fQ=='
 
 module.exports = defineConfig({
     server:{
+        host: 'localhost',
         port: 3000
     },
     plugins: [
